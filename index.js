ec0877129408fe0fe83304d29cd13bd9583dee90
Sesugh Asa <itsmason58@gmail.com>
2021-07-04 23:16:52 +0100

    hjk

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const error = require('./middleware/error');
 const winston = require('winston')
 const customerRoutes = require('./routes/customer-routes');
 //const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 app = express()
 require('./startup/config');
@@ -30,18 +23,7 @@ app.use(customerRoutes.routes);
 app.use(expressLayoutes);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
+
 //app.use(express.urlencoded({extended:true}));
 app.listen(process.env.PORT || "5000", () => {
   console.log(`"Running on ${process.env.PORT || "5000"}!`)
--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,11 @@
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const winston = require('winston');
-
+const uri = 'mongodb+srv://sesu:sesughasa1@cluster0.tu2xz.mongodb.net/sweet'
 module.exports=()=>{
-    mongoose.connect('mongodb://localhost/customerrDB',{
+    mongoose.connect(uri, {
         useNewUrlParser:true,
         useUnifiedTopology:true,
         useFindAndModify:true,
         useCreateIndex:true
     }).then(()=> winston.info('MongoDb connected succesfully...'));
-}*/
+}
