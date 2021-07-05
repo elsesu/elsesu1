1895ac2d01da7139441a29520b2a1d10ac701c7a
Sesugh Asa <itsmason58@gmail.com>
2021-07-05 15:54:08 +0100

    sd

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const path = require('path')
+//const cors = require('cors')
 const expressLayoutes = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+
 const error = require('./middleware/error');
 const winston = require('winston')
 const customerRoutes = require('./routes/customer-routes');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app = express()
 require('./startup/config');
-require('./startup/db');
+require('./startup/db')();
 //require('./startup/logging')();
-require('./startup/validations');
+require('./startup/validations')();
+
+app.use(expressLayoutes);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,12 +27,4 @@ app.use(customerRoutes.routes);
   app.use(expressLayoutes);
   app.use(customerRoutes.routes);
 
-app.use(expressLayoutes);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(express.urlencoded({extended:true}));
-//app.listen(process.env.PORT || "5000", () => {
- // console.log(`"Running on ${process.env.PORT || "5000"}!`)
-//})
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
